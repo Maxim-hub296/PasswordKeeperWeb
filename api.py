46f7d6324b537a9b5a1060f37fe8c0679c239216
Arthax <itoppro11@gmail.com>
2024-11-09 22:54:41 +0300

    feat(api): Add pydantic model for add_password and Update Post method (add_password)

--- api.py
+++ api.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI, HTTPException
 from crypto import Crypto
 from database_manager import ApiDatabaseManager
+from pydantic import BaseModel, Field, HttpUrl
+
+
+class CreatePassword(BaseModel):
+    site: HttpUrl = Field(..., description="The website name")
+    password: str = Field(..., description="The password")
+
 
 app = FastAPI()
 db = ApiDatabaseManager()
@@ -33,12 +40,16 @@ async def get_user(api_key: str):
 
 
 @app.post("/api/v1/passwords/add/{api_key}")
-async def add_password(api_key: str, site: str, password: str):
+async def add_password(api_key: str, password_data: CreatePassword) -> dict:
     try:
         user_name = db.get_user_by_api_key(api_key)
         user_password = db.get_password_by_name(user_name)
         if user_name:
-            db.save_password(user_name, site, Crypto.encrypt(password, user_password))
+            db.save_password(
+                user_name,
+                str(password_data.site),
+                Crypto.encrypt(password_data.password, user_password),
+            )
             return {"message": "Password added successfully"}
         else:
             raise HTTPException(status_code=404, detail="User not found")
